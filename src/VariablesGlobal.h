94b36361a88c8115befb8302bbdf81fa41400f0f
ArturKrestian <119724692+ArturKrestian@users.noreply.github.com>
2023-01-11 16:30:50 +0100

    Update VariablesGlobal.h

--- src/VariablesGlobal.h
+++ src/VariablesGlobal.h
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 
-const int lcd_Rows=4;
-const int lcd_Columns=20;
+const int lcd_Rows=4;           // ilość lini wyświetlacza
+const int lcd_Columns=20;       // ilość kolumn wyświetlacza
+
+//const int submenuCount=5;       // głębokość podmenu
+//int lastIndexMenu[submenuCount];// tablice z indeksami menu poziom wyższy
+
 
 volatile int sec = 0;
 volatile int miliSec = 0;
