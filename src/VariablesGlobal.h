bfde548bd5057419d0912eaa7a7d2544b2eac7ef
ArturKrestian <119724692+ArturKrestian@users.noreply.github.com>
2023-01-12 02:16:54 +0100

    Update VariablesGlobal.h

--- src/VariablesGlobal.h
+++ src/VariablesGlobal.h
@@ -3,26 +3,12 @@
 const int lcd_Rows=4;           // ilość lini wyświetlacza
 const int lcd_Columns=20;       // ilość kolumn wyświetlacza
 
-String mainLcdValue[4];
+String mainLcdValue[lcd_Rows];
+
 bool isMenuLock = false;
 int menuSelected = -1;
 bool isLcdRefresh = false;
 int demoSpeed = 0;
 
-volatile int sec = 0;
-volatile int miliSec = 0;
 
-struct MenuItem
-{
-    const char * name;
-    int subMenu;
-};
-
-struct MenuValue
-{
-    String name[4];
-    int indexSelect;
-    bool isOk;
-    bool isDisplay;
-};
 
